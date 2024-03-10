aed084a937e46f461edf03731ba2666f97cc1fa3
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-10 10:29:46 -0500

    Import of the group of floors

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,42 +1,18 @@
 import { OrbitControls } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import Floor from "./floor/FloorGroup";
 
 const Experience = () => {
   const boxRef = useRef(null);
-
-  useFrame((state, delta) => {
-    boxRef.current.rotation.x += 1 * delta;
-  })
 
   return (
     <>
       <ambientLight />
       <directionalLight position={[10, 10, 5]} />
       <OrbitControls makeDefault />
-      <mesh ref={boxRef}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="purple" />
-      </mesh>
+      <Floor />
     </>
   )
 }
 
 export default Experience;
-
-// // Este es el controlador principal
-
-// const Experience = ({title, subtitle}) => {
-//   return(
-//     <div>
-//       <h1 className="title"> {title} </h1>
-//       <span> {subtitle} </span>
-//     </div>
-//   )
-// }
-
-// // Exporta un valor unico
-// export default Experience; 
-
-// // Exporta multiples valores
-// // export {add, multiply, substract, divide}; 
