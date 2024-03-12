2125a2d168db256910b092b6658d7d7659a533fb
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-11 19:41:27 -0500

    Add textures to boxGeometry objects

--- src/floor/AnimatedFloorGroup.jsx
+++ src/floor/AnimatedFloorGroup.jsx
@@ -1,21 +1,59 @@
+import { useTexture } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import React, { useRef } from "react";
+import { RepeatWrapping } from "three";
 
 export default function FloorGroup() {
   const groupRef = useRef(null);
+
+  const PATH = "/assets/textures/floor/";
+
+  function createTexture(textureName) {
+    const texture = useTexture({
+      map: PATH + textureName + '_diff_1k.jpg',
+      displacementMap: PATH + textureName + '_disp_1k.png',
+      normalMap: PATH + textureName + '_nor_gl_1k.jpg',
+      roughnessMap: PATH + textureName + '_rough_1k.jpg',
+    });
+
+    // texture.map.repeat.set(4, 64);
+    // texture.map.wrapS = RepeatWrapping;
+    // texture.map.wrapT = RepeatWrapping;
+
+    // texture.displacementMap.repeat.set(4, 64);
+    // texture.displacementMap.wrapS = RepeatWrapping;
+    // texture.displacementMap.wrapT = RepeatWrapping;
+
+    // texture.normalMap.repeat.set(4, 64);
+    // texture.normalMap.wrapS = RepeatWrapping;
+    // texture.normalMap.wrapT = RepeatWrapping;
+
+    // texture.roughnessMap.repeat.set(4, 64);
+    // texture.roughnessMap.wrapS = RepeatWrapping;
+    // texture.roughnessMap.wrapT = RepeatWrapping;
+
+    return texture;
+  }
+
+  const coastSandTexture = createTexture('coast_sand_01');
+  const coastSandRocksTexture = createTexture('coast_sand_rocks_02');
+  const coastStoneWallTexture = createTexture('coral_stone_wall');
+  const gravelFloor02Texture = createTexture('gravel_floor_02');
+  const grayRocksTexture = createTexture('gray_rocks');
+
 
   useFrame(({ clock }) => {
     groupRef.current.position.y = Math.sin(clock.getElapsedTime());
   });
 
   const floorsProperties = [
-    { height: 0, material: <meshStandardMaterial color="purple" /> },
-    { height: 5, material: <meshLambertMaterial color="purple" /> },
-    { height: 10, material: <meshMatcapMaterial color="purple" /> },
-    { height: 15, material: <meshNormalMaterial color="purple" /> },
-    { height: 20, material: <meshPhongMaterial color="purple" /> },
-    { height: 25, material: <meshPhysicalMaterial color="purple" /> },
-    { height: 30, material: <meshToonMaterial color="purple" /> },
+    { height: 0, material: <meshStandardMaterial {...coastSandTexture} /> },
+    { height: 5, material: <meshLambertMaterial {...coastSandRocksTexture} /> },
+    { height: 10, material: <meshMatcapMaterial {...coastStoneWallTexture} /> },
+    { height: 15, material: <meshNormalMaterial {...gravelFloor02Texture} /> },
+    { height: 20, material: <meshPhongMaterial {...grayRocksTexture} /> },
+    { height: 25, material: <meshPhysicalMaterial {...coastSandTexture} /> },
+    { height: 30, material: <meshToonMaterial {...grayRocksTexture} /> },
   ];
 
   const floors = [];
