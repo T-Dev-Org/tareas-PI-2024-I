d0614aa6647b2e559ba111986bb2ab373897d486
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-31 10:38:24 -0500

    security commit Perf (see performance monitor)

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,10 +2,12 @@ import { OrbitControls } from "@react-three/drei";
 import World from "./world/World";
 import Lights from "./lights/lights";
 import Environments from "./environments/Environments";
+import { Perf } from "r3f-perf";
 
 const Experience = () => {
     return (
         <>
+            <Perf position="top-left" />
             <Environments />
             <Lights />
             <OrbitControls makeDefault />
